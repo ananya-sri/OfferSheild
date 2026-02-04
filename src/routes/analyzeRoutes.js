f926ab98c63d36a94febd3a7d41261e2b54f365f
ananya-sri <ananyasrivastava402@gmail.com>
2026-02-04 21:10:38 +0530

    Remove backend code from root and keep it inside backend folder

--- src/routes/analyzeRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-import { analyzeText } from "../controllers/analyzeController.js";
-
-const router = express.Router();
-router.post("/text", analyzeText);
-export default router;
-
